1520cd0bd1e891ccb9b469025d9b6edf259a0731
fummyolalekan <fummyolalekan@gmail.com>
2022-06-06 19:14:08 -0700

    Added 9-print_comb.c file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 100; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		putchar((num % 10) + '0');
-		if (num != 99)
+		putchar(num);
+		if (num != '9')
 		{
 			putchar(',');
 			putchar(' ');
