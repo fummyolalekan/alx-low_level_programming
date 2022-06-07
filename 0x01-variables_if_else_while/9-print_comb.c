3b3ea62f88df7ac9ec297c1b08a0f043aa46f352
fummyolalekan <fummyolalekan@gmail.com>
2022-06-06 18:52:18 -0700

    Added 9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 100; num++)
+	{
+		putchar((num / 10) + '0');
+		putchar((num % 10) + '0');
+		if (num != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
