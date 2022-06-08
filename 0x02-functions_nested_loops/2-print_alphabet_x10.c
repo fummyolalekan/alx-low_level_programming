37886ed9a86e01eacd0626cb97a0f8c9515f207c
fummyolalekan <fummyolalekan@gmail.com>
2022-06-08 13:22:00 -0700

    Added 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		putchar('\n');
+	}
+}
