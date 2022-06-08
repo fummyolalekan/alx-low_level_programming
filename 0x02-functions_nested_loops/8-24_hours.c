d1dba83258e3711c13c850b4e7cd462b609bc60c
fummyolalekan <fummyolalekan@gmail.com>
2022-06-08 16:00:11 -0700

    Added 8-24_hours.c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,6 +6,7 @@
 void jack_bauer(void)
 {
 	int i, j;
+
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
@@ -26,12 +27,13 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(j + '0');
 			}
-			else if (j > 10)
+			else if (j >= 10)
 			{
 				_putchar(':');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
