d972ad0fc8113d79df6afee034be07557b03691d
fummyolalekan <fummyolalekan@gmail.com>
2022-06-10 07:55:58 -0700

    added 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - is a function used to check for uppercase alphabets
+ * @c: an integer parameter
+ * Return: something but success
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
