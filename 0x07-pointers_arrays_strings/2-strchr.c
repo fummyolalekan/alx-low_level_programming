99da9bfaecd8bb8c9d720eb83a4037d9f3387883
fummyolalekan <fummyolalekan@gmail.com>
2022-06-21 14:37:57 -0700

    third task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,13 @@
 
 char *_strchr(char *s, char c)
 {
-		int a;
-
-		while (1)
-		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
-		}
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+		s++;
+	}
+	return (s + 1);
 }
