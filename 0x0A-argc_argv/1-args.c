4e70c93eb1ac341dbeb8d2f563ac83774939c488
fummyolalekan <fummyolalekan@gmail.com>
2022-06-24 08:45:42 -0700

    first task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this checks the code
+ * @argv: This is the character to print
+ * @argc: This is the character to print
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
