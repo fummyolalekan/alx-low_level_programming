9afbbf5d5ca4b237388ccb36a24147838012f394
fummyolalekan <fummyolalekan@gmail.com>
2022-07-19 06:42:03 -0700

    first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all of the elements of a linked list.
+ * @h: Pointer to a linked list head.
+ * Return: The number of total nodes in the linked list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node;
+
+	node = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
