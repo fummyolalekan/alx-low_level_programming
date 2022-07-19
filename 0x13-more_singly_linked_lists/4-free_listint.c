5fa724bbffe5238ccb8c51822ee58c91e5e7d879
fummyolalekan <fummyolalekan@gmail.com>
2022-07-19 14:21:28 -0700

    fifth task

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,7 +3,9 @@
 /**
  * free_listint - Frees a list of nodes
  * @head: Pointer to the first element of the list
+ * Return: nothing
  */
+void free_listint(listint_t *head)
 {
 	listint_t *current;
 
