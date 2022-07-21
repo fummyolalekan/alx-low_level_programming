96e52fb32bafb8b153ca7d1bcae2e122c6a39fd2
fummyolalekan <fummyolalekan@gmail.com>
2022-07-21 12:53:47 -0700

    first task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,29 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len, i, j, sum, pow;
-	int base;
-
-	base = 2;
-	sum = 0;
-	pow = 1;
-
+	unsigned int value = 0;
 
 	if (b == NULL)
 		return (0);
-	for (len = 0; b[len] != '\0'; len++)
-		;
-	if (len == 1 && (b[0] == '0' || b[0] == '1'))
-		return ((b[0] - 48));
-	for (i = 0; b[i] != '\0'; i++)
+	while (*b != '\0')
 	{
-		if (b[i] != '0' && b[i] != '1')
+		value = value << 1;
+		if (*b != '1' && *b != '0')
 			return (0);
-		for (j = len - 1; j > 0; j--)
-			pow = pow * base;
-		sum = sum + (pow * (b[i] - 48));
-		len--;
-		pow = 1;
+		else if (*b == '1')
+			value = value | 1;
+		b++;
 	}
-	return (sum);
+	return (value);
 }
