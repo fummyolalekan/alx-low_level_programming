ec6fc8a321d71d975e657d767f0ffaddad01b71e
fummyolalekan <fummyolalekan@gmail.com>
2022-07-22 14:11:29 -0700

    first task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,6 @@
  * Return: the converted number, or 0 if b is NULL
  * or there is one or more chars in the string b that is not 0 or 1
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
@@ -14,6 +13,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
+
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
